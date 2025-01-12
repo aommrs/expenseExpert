20cfa05adffe5571ef25efe346d2dcec9ddcace0
coderSpy007 <149516986+coderSpy007@users.noreply.github.com>
2025-01-12 18:32:22 +0700

    complete all

--- src/app/components/logo/page.tsx
+++ src/app/components/logo/page.tsx
@@ -1,18 +1,27 @@
 "use client";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 export default function Logo() {
   const router = useRouter();
   return (
     <>
-      <Image
-        className="hover:cursor-pointer"
-        src="/images/expense-logo.svg"
-        alt="Logo Image"
-        width={120}
-        height={120}
-        onClick={() => router.push("/")}
-      />
+      <div className="flex justify-between items-center px-[2rem] md:px-[4rem]">
+        <Image
+          className="hover:cursor-pointer"
+          src="/images/expense-logo.svg"
+          alt="Logo Image"
+          width={120}
+          height={120}
+          onClick={() => router.push("/")}
+        />
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </>
   );
 }
